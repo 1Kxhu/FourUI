d08bba39eb622602503d6922520e8a7a7fd56120
BuildTools <unconfigured@null.spigotmc.org>
2023-07-11 02:24:57 +0200

    Update FourButton to handle fillcolor and backcolor differently, allowing the user to put the button on a panel control.

--- FourButton.cs
+++ FourButton.cs
@@ -1,40 +1,69 @@
 ﻿using System;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Drawing.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FourUI
 {
-
-    //checking if push works v2
     public class FourButton : Button
     {
-        private int rippleRadius = 0;
-
         private int cornerRadius = 7; // default corner radius value
         private int borderWidth = 2; // border width
         private Color backColor2 = Color.FromArgb(32, 32, 32); // default BackColor2 value
         private Color originalOriginalColor;
         private Color originalColor;
-        private Color BackColor2;
         private Color pressCol;
+
+        public Color BackColor2
+        {
+            get { return backColor2; }
+            set
+            {
+                backColor2 = value;
+                Refresh(); // redraw the button with the new BackColor2
+            }
+        }
+
+        public Color PressColor
+        {
+            get { return pressCol; }
+            set
+            {
+                pressCol = value;
+                Refresh(); // redraw the button with the new press color
+            }
+        }
+
+        public Color TransitionColor1
+        {
+            get { return originalColor; }
+            set
+            {
+                originalColor = value;
+                Refresh(); // redraw the button with the new original color
+                originalOriginalColor = value;
+            }
+        }
+
+        public Color TransitionColor2
+        {
+            get { return backColor2; }
+            set
+            {
+                backColor2 = value;
+                Refresh(); // redraw the button with the new BackColor2
+            }
+        }
 
         public FourButton()
         {
-            //
-            //those are the default values, you can change them in the properties when you add the dll to your project.
-            //
             TransitionColor1 = Color.FromArgb(32, 32, 32);
             TransitionColor2 = Color.FromArgb(41, 41, 41);
             pressCol = Color.FromArgb(244, 244, 244); // Assign pressCol here
-            PressColor = Color.FromArgb(244, 244, 244);
 
-            // Assign a default value to originalOriginalColor
             originalOriginalColor = TransitionColor1;
-            BackColor2 = backColor2;
 
             if (BackColor2.IsEmpty || !BackColor2.IsEmpty)
                 BackColor2 = backColor2;
@@ -47,21 +76,12 @@ namespace FourUI
             MouseEnter += CustomButton_MouseEnter;
             MouseLeave += CustomButton_MouseLeave;
             MouseClick += Ye;
+            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            SetStyle(ControlStyles.Opaque, true);
             SetStyle(ControlStyles.ResizeRedraw, true);
-            // Subscribe to the BackColorChanged event of the targetForm
-        }
-
-        private Color a = Color.White;
-
-        private void TargetForm_BackColorChanged(object sender, EventArgs e)
-        {
-            // Update the backgroundColor variable with the new BackColor
-            a = Parent.BackColor;
         }
 
         private CancellationTokenSource cancellationTokenSource; // Used to cancel the animation
-
-
         private async void Ye(object sender, EventArgs e)
         {
             // Cancel any ongoing animation
@@ -78,11 +98,10 @@ namespace FourUI
                 int deltaG = (int)Math.Round((originalOriginalColor.G - originalColor.G) / (double)steps);
                 int deltaB = (int)Math.Round((originalOriginalColor.B - originalColor.B) / (double)steps);
 
-
                 // Instant transition to the press color
                 originalColor = pressCol;
                 Invalidate();
-                System.Windows.Forms.Application.DoEvents();
+                Application.DoEvents();
 
                 Point mousePosition = PointToClient(MousePosition);
                 while (MouseButtons != MouseButtons.None && ClientRectangle.Contains(mousePosition))
@@ -94,7 +113,7 @@ namespace FourUI
 
                     originalColor = Color.FromArgb(newR, newG, newB); // Update the original color
                     Invalidate(); // Redraw the control
-                    System.Windows.Forms.Application.DoEvents(); // Allow the UI to update
+                    Application.DoEvents(); // Allow the UI to update
 
                     await Task.Delay(TimeSpan.FromMilliseconds(delay), cancellationTokenSource.Token); // Delay the animation
 
@@ -156,7 +175,6 @@ namespace FourUI
             }
         }
 
-
         private void CustomButton_MouseLeave(object sender, EventArgs e)
         {
             // Cancel any ongoing animation
@@ -178,7 +196,7 @@ namespace FourUI
 
                 originalColor = Color.FromArgb(newR, newG, newB); // update the originalColor
                 Invalidate(); // redraw the control
-                System.Windows.Forms.Application.DoEvents(); // allow the UI to update
+                Application.DoEvents(); // allow the UI to update
 
                 Thread.Sleep(delay);
 
@@ -217,11 +235,9 @@ namespace FourUI
                 int newG = originalColor.G + deltaG;
                 int newB = originalColor.B + deltaB;
 
-                if (newR < 256 && newB < 256 && newG < 256)
-                    originalColor = Color.FromArgb(newR, newG, newB); // update the originalColor
-                if (newR < 256 && newB < 256 && newG < 256)
-                    BackColor = originalColor; // update the BackColor
-                System.Windows.Forms.Application.DoEvents(); // allow the UI to update
+                originalColor = Color.FromArgb(newR, newG, newB); // update the originalColor
+                Invalidate(); // redraw the control
+                Application.DoEvents(); // allow the UI to update
 
                 Thread.Sleep(delay);
             }
@@ -229,113 +245,75 @@ namespace FourUI
             Invalidate(); // redraw the control with the final color
         }
 
-        public int CornerRadius
-        {
-            get { return cornerRadius; }
-            set
-            {
-                if (value >= 0)
-                {
-                    cornerRadius = value;
-                    Refresh(); // redraw the button with the new corner radius
-                }
-            }
-        }
-
-        public Color TransitionColor1
-        {
-            get { return originalColor; }
-            set
-            {
-                originalColor = value;
-                Refresh(); // redraw the button with the new originalColor
-                originalOriginalColor = value;
-            }
-        }
-
-        public Color TransitionColor2
-        {
-            get { return backColor2; }
-            set
-            {
-                backColor2 = value;
-                Refresh(); // redraw the button with the new BackColor2
-            }
-        }
-
-        public Color PressColor
-        {
-            get { return pressCol; }
-            set
-            {
-                pressCol = value;
-                Refresh(); // redraw the button with the new originalColor
-            }
-        }
-
-        public Color RippleColor { get; set; } = Color.White;
-
-        public int RippleRadius
-        {
-            get { return rippleRadius; }
-            set
-            {
-                rippleRadius = value;
-                Invalidate();
-            }
-        }
-
-
         protected override void OnPaint(PaintEventArgs pevent)
         {
-            Parent.BackColorChanged += TargetForm_BackColorChanged;
-            base.OnPaint(pevent);
+            // Skip the base implementation to remove the default button appearance
+            // base.OnPaint(pevent);
+
+            // Fill the background with the parent's background color
+            using (SolidBrush brush = new SolidBrush(Parent.BackColor))
+            {
+                pevent.Graphics.FillRectangle(brush, ClientRectangle);
+            }
+
+            // Add your custom painting code here
+            Rectangle rect = new Rectangle(ClientRectangle.Left, ClientRectangle.Top,
+                                           ClientRectangle.Width - 1, ClientRectangle.Height - 1);
 
             GraphicsPath path = new GraphicsPath();
             int arcSize = 2 * cornerRadius;
 
             // Define the main rounded rectangle path using Bézier curves
-            path.AddArc(borderWidth - 2, borderWidth, arcSize, arcSize, 180, 90);
-            path.AddArc(Width - arcSize - borderWidth, borderWidth, arcSize, arcSize, 270, 90);
-            path.AddArc(Width - arcSize - borderWidth, Height - arcSize - borderWidth, arcSize, arcSize, 0, 90);
-            path.AddArc(borderWidth - 2, Height - arcSize - borderWidth, arcSize, arcSize, 90, 90);
+            path.AddArc(rect.Left, rect.Top, arcSize, arcSize, 180, 90);
+            path.AddArc(rect.Right - arcSize, rect.Top, arcSize, arcSize, 270, 90);
+            path.AddArc(rect.Right - arcSize, rect.Bottom - arcSize, arcSize, arcSize, 0, 90);
+            path.AddArc(rect.Left, rect.Bottom - arcSize, arcSize, arcSize, 90, 90);
 
             path.CloseFigure();
 
-            // create the region for the button using the main rounded rectangle path
-            Region = new Region(path);
-
-            // draw the button using the main rounded rectangle path with anti-aliasing
-            using (SolidBrush brush = new SolidBrush(originalColor))
-            {
-                pevent.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
-                pevent.Graphics.FillPath(brush, path);
-            }
-
-
-
-
-            // draw the border outline
-            using (Pen borderPen = new Pen(Color.Transparent, borderWidth))
+            // Draw a border of backcolor, so there isnt a black square. (This is a workaround, because I couldn't come up with anything better, sorry!)
+            using (Pen borderPen = new Pen(this.BackColor, borderWidth + 74))
             {
                 pevent.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
                 pevent.Graphics.DrawPath(borderPen, path);
             }
 
+            // Fill the button with the desired color
+            using (SolidBrush brush = new SolidBrush(originalColor))
+            {
+                pevent.Graphics.FillPath(brush, path);
+            }
 
-
-
-
-            // draw the button text
-            TextRenderer.DrawText(pevent.Graphics, Text, Font, ClientRectangle, ForeColor,
+            // Draw the button text
+            TextRenderer.DrawText(pevent.Graphics, Text, Font, rect, ForeColor,
                 TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter);
         }
 
-        protected override void OnPaintBackground(PaintEventArgs pevent)
+        public int CornerRadius
         {
-            // Do nothing to prevent the base class from painting the background
+            get { return cornerRadius; }
+            set
+            {
+                cornerRadius = value;
+                Refresh(); // redraw the button with the new corner radius
+            }
         }
 
+        private static GraphicsPath CreateRoundedRectanglePath(Rectangle rectangle, int cornerRadius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            int arcSize = 2 * cornerRadius;
+
+            // Define the main rounded rectangle path using Bézier curves
+            path.AddArc(rectangle.Left, rectangle.Top, arcSize, arcSize, 180, 90);
+            path.AddArc(rectangle.Right - arcSize, rectangle.Top, arcSize, arcSize, 270, 90);
+            path.AddArc(rectangle.Right - arcSize, rectangle.Bottom - arcSize, arcSize, arcSize, 0, 90);
+            path.AddArc(rectangle.Left, rectangle.Bottom - arcSize, arcSize, arcSize, 90, 90);
+
+            path.CloseFigure();
+
+            return path;
+        }
 
     }
 }
